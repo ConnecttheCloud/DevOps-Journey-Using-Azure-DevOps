906d96ca69feaac76394a219ef6dd0f82f977b8a
shwe <shwe@gmail.com>
2024-01-19 00:48:42 +0800

    Lab1 Change

--- labs/1-Initial-Setup/scripts/create-terraform-storage.sh
+++ labs/1-Initial-Setup/scripts/create-terraform-storage.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-RESOURCE_GROUP_NAME="devops-journey-rg"
-STORAGE_ACCOUNT_NAME="devopsjourneyazuredevops"
+RESOURCE_GROUP_NAME="Devops"
+STORAGE_ACCOUNT_NAME="devopsjourneystorageacct"
+LOCATION="SouthEastAsia"
 
 # Create Resource Group
-az group create -l uksouth -n $RESOURCE_GROUP_NAME
+# az group create -l $LOCATION -n $RESOURCE_GROUP_NAME   ##already created when devops pipline authenticate with Azure
 
 # Create Storage Account
-az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l uksouth --sku Standard_LRS
+az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l $LOCATION --sku Standard_LRS
 
 # Create Storage Account blob
 az storage container create  --name tfstate --account-name $STORAGE_ACCOUNT_NAME
